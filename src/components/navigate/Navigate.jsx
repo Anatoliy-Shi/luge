a46077e6dd137f1eab6de54459a58840830c09ef
Анатолий Шипилов <asmatol@yandex.ru>
2022-07-30 20:14:17 +0300

    add active link

--- src/components/navigate/Navigate.jsx
+++ src/components/navigate/Navigate.jsx
@@ -1,28 +1,57 @@
 import s from './Navigate.module.css'
-import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 
 export const Navigate = () => {
+    let activeStyle = {
+        color: "#faf9ea",
+    };
+
     return (
 
         <nav className={s.navigate}>
             <ul className={s.listItems}>
                 <li className={s.listItem}>
-                    <Link to='/'>Главная</Link>
+                    <NavLink to='/'
+                             style={({ isActive }) =>
+                                 isActive ? activeStyle : undefined
+                    }
+                                 className={s.link}>Главная</NavLink>
+
                 </li>
                 <li className={s.listItem}>
-                    <Link to='/documents'>Документы</Link>
+                    <NavLink to='/documents'
+                             style={({ isActive }) =>
+                                 isActive ? activeStyle : undefined
+                             }
+                             className={s.link}>Документы</NavLink>
                 </li>
                 <li className={s.listItem}>
-                    <Link to='/news'>Новости</Link>
+                    <NavLink to='/news'
+                             style={({ isActive }) =>
+                                 isActive ? activeStyle : undefined
+                             }
+                             className={s.link}>Новости</NavLink>
                 </li>
                 <li className={s.listItem}>
-                    <Link to='/team'>Команда</Link>
+                    <NavLink to='/team'
+                             style={({ isActive }) =>
+                                 isActive ? activeStyle : undefined
+                             }
+                             className={s.link}>Команда</NavLink>
                 </li>
                 <li className={s.listItem}>
-                    <Link to='/history'>История</Link>
+                    <NavLink to='/history'
+                             style={({ isActive }) =>
+                                 isActive ? activeStyle : undefined
+                             }
+                             className={s.link}>История</NavLink>
                 </li>
                 <li className={s.listItem}>
-                    <Link to='/competition-results'>Результаты соревнования</Link>
+                    <NavLink to='/competition-results'
+                             style={({ isActive }) =>
+                                 isActive ? activeStyle : undefined
+                             }
+                             className={s.link}>Результаты соревнований</NavLink>
                 </li>
             </ul>
         </nav>
